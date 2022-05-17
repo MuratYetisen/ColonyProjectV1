941367bb76baa14e44de5b2edf25e74a15cc6b4e
Murat Yetişen <101714901+MuratYetisen@users.noreply.github.com>
2022-05-17 19:35:04 +0300

    17.05.22

--- Assets/Scripts/LoginMain.cs
+++ Assets/Scripts/LoginMain.cs
@@ -1,48 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Windows.Input;
+using UnityEngine.UI;
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
-using UnityEngine.UI;
-
 
 public class LoginMain : MonoBehaviour
 {
-  [SerializeField]  InputField _emailRegister, _passwordRegister, _usernameRegister, _repeatPasswordRegister;
-    [SerializeField] InputField  _passwordLogin, _usernameandEmailLogin;
-    [SerializeField] Toggle _rememberToggle;
+
+    [SerializeField]
+    InputField _emailRegister, _passwordRegister, _usernameRegister, _repeatPasswordRegister;
+    [SerializeField]
+    InputField _passwordLogin, _usernameAndEmailLogin;
+    [SerializeField]
+    Button _RegisterButton, _LoginButton;
+    [SerializeField]
+    Text _resultText;
     [Header("Guest Login Settings")]
-    [SerializeField] bool _GuestLogin;
-    [SerializeField] Button _LoginOrRegisterButton;
-    [SerializeField] Text _registerOrloginText,_alreadyText;
-    [SerializeField] GameObject _emailRegisterGO, _passwordRegisterGo, _usernameRegisterGo, _repeatPasswordRegisterGo, _passwordLoginGo, _usernameandEmailLoginGo, _rememberToggleGo;
+    [SerializeField]
+    bool _guestLogin;
+    [SerializeField]
+    GameObject _registerPanel, _loginPanel;
+    [SerializeField] Animator _animator;
+
+
+    private void Start()
+    {
+        SwitchLoginOrRegister();
+    }
 
     #region RegisterLogin System
+
     public void LoginEmail()
     {
-        //PlayFabClientAPI.LoginWithEmailAddress(new LoginWithEmailAddressRequest() { Email = _usernameandEmailLogin.text, Password = _passwordLogin.text }, Result => { Debug.Log("Giris Basarili"); }, Error =>
-        //{
-        //    Debug.Log("Giris Basarisiz");
-        //});
-        Debug.Log("aaa");
-            }
+        PlayFabClientAPI.LoginWithEmailAddress(new LoginWithEmailAddressRequest()
+        {
+            Email = _usernameAndEmailLogin.text,
+            Password = _passwordLogin.text
 
-    private void Awake()
-    {
-        SwitchLoginorRegister();
-    }
-    public void LoginUsername()
-    {
-        PlayFabClientAPI.LoginWithPlayFab(new LoginWithPlayFabRequest() { Username = _usernameRegister.text,Password=_passwordLogin.text }, Result => { Debug.Log("Giris Basarili"); }, Error =>
+        },
+        Result =>
+        {
+            Debug.Log("Giris basarili");
+
+        },
+        Error =>
         {
             Debug.Log("Giris Basarisiz");
-        });
 
-        } 
-      public void SwitchLoginType()
+        });
+        Debug.Log("aaaaa");
+    }
+
+
+    public void LoginUsername()
     {
-        if (_usernameandEmailLogin.text.IndexOf('@')>0)
+        PlayFabClientAPI.LoginWithPlayFab(new LoginWithPlayFabRequest()
+        {
+            Username = _usernameAndEmailLogin.text,
+            Password = _passwordLogin.text
+
+        },
+        Result =>
+        {
+            Debug.Log("Giris basarili");
+
+        },
+        Error =>
+        {
+            Debug.Log("Giris Basarisiz");
+
+        }); ;
+    }
+
+    public void SwitchLoginType()
+    {
+        if (_usernameAndEmailLogin.text.IndexOf('@') > 0)
         {
             LoginEmail();
         }
@@ -51,58 +84,94 @@ public class LoginMain : MonoBehaviour
             LoginUsername();
         }
     }
+
+
     public void Register()
     {
-        //PlayFabClientAPI.RegisterPlayFabUser(new RegisterPlayFabUserRequest() { Username=_usernameRegister.text,Email=_emailRegister.text,Password=_passwordRegister.text }, Result => { Debug.Log("Giris Basarili"); }, Error =>
-        //{
-        //    Debug.Log("Giris Basarisiz");
-        //});
-        Debug.Log("bbb");
+        PlayFabClientAPI.RegisterPlayFabUser(new RegisterPlayFabUserRequest()
+        {
+            Username = _usernameRegister.text,
+            Email = _emailRegister.text,
+            Password = _passwordRegister.text
+
+        },
+        Result =>
+        {
+            _animator.Play("Success");
+            Debug.Log("Kayit basarili");
+
+        },
+        Error =>
+        {
+            Debug.Log("Kayit Basarisiz");
+            _animator.Play("Fail");
+        }); ;
+        Debug.Log("bbbbb");
+
     }
+
     public void RememberMe()
     {
-        if (_rememberToggle.isOn)
-        {
-            PlayerPrefs.SetString("emailOrUsername", _usernameandEmailLogin.text);
-            PlayerPrefs.SetString("password", _passwordLogin.text);
-        }
-    } 
+
+
+
+        PlayerPrefs.SetString("emailOrUsername", _usernameAndEmailLogin.text);
+        PlayerPrefs.SetString("passowrd", _passwordLogin.text);
+
+
+        //PlayFabClientAPI.LoginWithAndroidDeviceID(new LoginWithAndroidDeviceIDRequest()
+        //{
+
+
+        //},
+        //Result =>
+        //{
+
+        //},
+        //Error =>
+        //{
+
+        //});
+    }
+
+
+
     public void PlayGuest()
     {
-        PlayFabClientAPI.LoginWithAndroidDeviceID(new LoginWithAndroidDeviceIDRequest() { CreateAccount= _GuestLogin,}, Result => { }, Error => { });
-    }
-    #endregion
-    public void SwitchLoginorRegister()
-    {
-        switch (_emailRegisterGO.activeInHierarchy)
+        PlayFabClientAPI.LoginWithAndroidDeviceID(new LoginWithAndroidDeviceIDRequest()
         {
-            case false:
-                _usernameandEmailLoginGo.SetActive(false);
-                _passwordLoginGo.SetActive(false);
-                _emailRegisterGO.SetActive(true);
-                _passwordRegisterGo.SetActive(true);
-                _repeatPasswordRegisterGo.SetActive(true);
-                _usernameRegisterGo.SetActive(true);
-                _registerOrloginText.text = "Register";
-                _alreadyText.text = "Login";
-                _rememberToggleGo.SetActive(false);
-                _LoginOrRegisterButton.onClick.AddListener(SwitchLoginType);
-                _LoginOrRegisterButton.onClick.AddListener(Register);
+            CreateAccount = _guestLogin,
+            AndroidDeviceId = SystemInfo.deviceUniqueIdentifier
 
+
+        },
+        Result =>
+        {
+            Debug.Log("Misafir Girisi basarili");
+        },
+        Error =>
+        {
+            Debug.Log("Misafir Girisi basarisiz");
+        });
+    }
+
+    public void SwitchLoginOrRegister()
+    {
+        switch (_registerPanel.activeInHierarchy)
+        {
+            case true:
+                _loginPanel.SetActive(true);
+                _registerPanel.SetActive(false);
                 break;
             default:
-                _usernameandEmailLoginGo.SetActive(true);
-                _passwordLoginGo.SetActive(true);
-                _emailRegisterGO.SetActive(false);
-                _passwordRegisterGo.SetActive(false);
-                _repeatPasswordRegisterGo.SetActive(false);
-                _usernameRegisterGo.SetActive(false);
-                _registerOrloginText.text = "Login";
-                _alreadyText.text = "Register";
-                _LoginOrRegisterButton.onClick.AddListener(Register);
-                _LoginOrRegisterButton.onClick.AddListener(SwitchLoginType);
+                _loginPanel.SetActive(false);
+                _registerPanel.SetActive(true);
                 break;
         }
-       
     }
+
+
+    #endregion
+
+
 }
